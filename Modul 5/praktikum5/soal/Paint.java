5420f80248037dd0c034703705754c02ed51f927
frdvndb <89407930+frdvndb@users.noreply.github.com>
2022-11-28 23:56:08 +0700

    Add files via upload

---
+++ Modul 5/praktikum5/soal/Paint.java
@@ -0,0 +1,17 @@
+package praktikum5.soal;
+
+public class Paint
+{
+    private double coverage;
+
+    public Paint(double c)
+    {
+        coverage = c;
+    }
+
+    public double amount(Shape s)
+    {
+        System.out.println ("Computing amount for " + s);
+        return s.area()/coverage;
+    }
+}
